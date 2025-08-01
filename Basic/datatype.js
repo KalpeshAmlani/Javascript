4018747be96d103fd73b0ecc95e41f4adcb0cbbe
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-08-01 18:32:10 +0530

    datatype

---
+++ Basic/datatype.js
@@ -0,0 +1,15 @@
+let name = "Kalpesh Amlani";
+const age = 30;
+let isEmployed = true;
+
+
+// number
+// Integer
+// Float
+// BigInt
+ // null
+ // undefined
+ // symbol = Symbol('unique');
+
+ console.log(typeof undefined);
+ log(typeof null);
