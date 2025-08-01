6f31989888618ce5a737c1945ab19eacdc72f332
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-08-01 15:33:16 +0530

    variable

---
+++ Basic/varible.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountName = "John Doe"
+var accountBalance = 1000.00
+accountCity = "New York"
+// accountId = "67890"; // Reassigning accountId
+
+accountName = 'KAlpesh Amlani' // Reassigning accountName is allowed
+accountBalance = 1500.00 // Reassigning accountBalance is allowed
+accountCity = "Los Angeles" // Reassigning accountCity is allowed
+console.table([accountId, accountName, accountBalance, accountCity]);
