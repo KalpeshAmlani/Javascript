3ac19854e56ae48b23afb2af8562ce2454f8e5a5
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-08-04 18:45:15 +0530

    Comparsion

---
+++ Basic/comparson.js
@@ -0,0 +1,19 @@
+// console.log( 2>1 );
+// console.log( 2 < 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 < 1 );
+// console.log( 2 != 1 );
+
+// console.log( "2" > 1 );
+// console.log( "02" > 1 );
+
+
+// console.log(null > undefined); // Output: false
+// console.log(null > 0);
+// console.log(null == undefined); // Output: true
+// console.log(null>= 0);
+
+console.log("2" == 2); // Output: true (type coercion)
+console.log("2" === 2); // Output: false (strict equality, no type coercion)
+console.log("2" != 2); // Output: false (type coercion)
+console.log("2" !== 2); // Output: true (strict inequality, no type coercion)
