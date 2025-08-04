d6ad0c78239f11bbd90000d0fa15d804623053be
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-08-04 18:32:23 +0530

    Opertor

---
+++ Basic/opertor.js
@@ -0,0 +1,17 @@
+let str1 = "Kalpesh";
+let str2 = "Amlani";
+let str3 = str1 + " " + str2; 
+// console.log(str3); 
+// console.log(str1 + " " + str2); 
+
+// console.log(1 + 2); // Output: 3
+// console.log("1" + "2"); 
+// console.log("1" + 2); // Output: "12" (string concatenation)
+// console.log(1 + "2"); // Output: "12" (string concatenation)
+// console.log("1" + 1 + 2); // Output: "12" (string concatenation)
+// console.log(1 + 1 + "2"); // Output: "12" (string concatenation)
+
+let gameCounter = 100;
+// ++gameCounter; // Incrementing the game counter
+gameCounter ++; // Incrementing the game counter again
+console.log(gameCounter); // Output: 101
