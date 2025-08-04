7701270da16a0a7957b778134be124c8f8263769
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-08-04 15:40:07 +0530

    test update

---
+++ Basic/dtconvertion.js
@@ -0,0 +1,17 @@
+// let score ="55avs"
+// let score =null
+// let score =undefined
+// let score =true
+// let score = "Kalpesh"; // Example of a string that can be converted to a number
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isloggedin = 1;
+let isloggedin = "Kalpesh";
+let isLoggedInBoolean = Boolean(isloggedin);
+console.log(isLoggedInBoolean);
