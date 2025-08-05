6c24823e5f2247fe1b965b9a0614b17727632d22
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-08-05 10:34:32 +0530

    dtsum

---
+++ Basic/datatypesummary.js
@@ -0,0 +1,19 @@
+// Primitive data types in JavaScript
+// This file summarizes the basic data types in JavaScript.
+// It includes examples and explanations of each type
+// 7 type of data types in JavaScript
+
+
+// String
+// Number
+// Boolean
+//null
+// undefined
+// Symbol
+// BigInt
+
+ 
+// Reference data types ( not primitive types)
+// Object
+// Array
+// Function
