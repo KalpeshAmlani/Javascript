5c36f0f3daf4d6675bb5ff09bfd444a664095bfa
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-08-05 13:07:17 +0530

    update dts

--- Basic/datatypesummary.js
+++ Basic/datatypesummary.js
@@ -17,3 +17,30 @@
 // Object
 // Array
 // Function
+
+
+const score = 100; // Number
+const name = "John Doe"; // String
+const isActive = true; // Boolean
+const user = null;  // Null
+let age;    // Undefined
+const uniqueId = Symbol('123'); // Symbol
+const id = Symbol('123'); // Symbol
+// console.log( uniqueId === id); // false, symbols are unique
+
+// const bigNumber = BigInt(123456789012345678901234567890); // BigInt 
+
+const heros = ["Super  hero ", "Shaktiman", "Batman"]; // Array
+const person = { // Object
+  name: "John Doe",
+  age: 30,
+  isActive: true
+};
+function greet() { // Function
+  console.log("Hello, World!");
+}   
+
+const myFunction = function() { // Function expression
+  console.log("This is a function expression.");
+}
+console.log(typeof user); // "number"
