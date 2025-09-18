4d1f24a0f4b5fa24509d8512239f73bef2bd43c2
Kalpesh Amlani <kalpeshamlani1@gmail.com>
2025-09-18 14:54:06 +0530

    heap

--- Basic/heapstack.js
+++ Basic/heapstack.js
@@ -2,4 +2,17 @@ let myYoutubeChannel = "Kalpesh Amlani"; // String
 
 let anotherName = myYoutubeChannel 
 
-console.log(anotherName); // "Kalpesh Amlani"
+// console.log(anotherName); // "Kalpesh Amlani"
+
+
+let userOne = {
+    email:"kalpesh@panthinfi.com",
+    upi :    "kalpesh@icici"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "kalpesh@kalpeshamlani.com";
+
+console.log(userOne.email); 
+console.log(userTwo.email); 
